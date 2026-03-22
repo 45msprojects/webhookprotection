83c53fa346ecee8ad98950f801bb2a441fa69ba7
45msmachi <aurevix7@gmail.com>
2026-03-22 22:39:01 +0200

    webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const WEBHOOK_URL = process.env.MY_DISCORD_WEBHOOK;
+  if (req.method === 'POST') {
+    try {
+      await fetch(WEBHOOK_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(req.body)
+      });
+      return res.status(200).json({ success: true });
+    } catch (err) {
+      return res.status(500).json({ error: "Error" });
+    }
+  }
+  return res.status(405).json({ error: "YOU SMELL" });
+}
